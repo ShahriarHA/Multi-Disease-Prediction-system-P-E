ac199d8e2a5ee116ab32dea7cfb33a3a224b1507
ShahriarHA <shahriar08neub@gmail.com>
2024-01-16 21:48:23 +0600

    img_classifying

--- multi_disease_pre.py
+++ multi_disease_pre.py
@@ -13,7 +13,34 @@ Pneumonia_model = pickle.load(open('pneumonia-disease-pretrained-mode.sav','rb')
 
 eye_disease_model = pickle.load(open('eye-disease-pretrained-mode.sav','rb'))
 
+image_classification_model = pickle.load(open('image_classifying_preTrained_model.sav','rb'))
+
+
 # models prediction function
+# image classification model
+def pre_img_class(image_path,model):
+    individual_image_transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+    individual_image = Image.open(image_path).convert("RGB")
+    individual_image = individual_image_transform(individual_image)
+    individual_image = individual_image.unsqueeze(0)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
+    individual_image = individual_image.to(device)
+    model.eval()
+
+    with torch.no_grad():
+        output = model(individual_image)
+
+    _, predicted_class = torch.max(output, 1)
+    predicted_class = predicted_class.item()
+
+    return predicted_class
+
 # pneumonia prediction
 def predict_pneumonia(image_path, model):
     individual_image_transform = transforms.Compose([
@@ -80,14 +107,17 @@ def load_pneumonia_transfer_learning_model():
     chest_x_ray_image = st.file_uploader('Please uploade your chest x-ray image',type=['jpeg','png','jpg'])
     if chest_x_ray_image is not None:
         st.image(chest_x_ray_image, caption='Uploaded Image.')
-        # Predict the class
-        predicted_class = predict_pneumonia(chest_x_ray_image, Pneumonia_model)
-        if predicted_class == 0:
-            st.success("The image is predicted as Normal.")
-        elif predicted_class == 1:
-            st.error("The image is predicted as Pneumonia.")
+        # checking image class whether it is chest X-ray image
+        predict_image_class = pre_img_class(chest_x_ray_image,image_classification_model)
+        if(predict_image_class == 0):
+            # Predict the class Pneumonia or normal
+            predicted_class = predict_pneumonia(chest_x_ray_image, Pneumonia_model)
+            if predicted_class == 0:
+                st.success("The image is predicted as Normal.")
+            elif predicted_class == 1:
+                st.error("The image is predicted as Pneumonia.")
         else:
-            st.warning("Unable to determine the prediction.")
+            st.warning("Unable to determine the prediction. Because uploaded image is not chest X-ray image.")
     else:
         st.warning('Please upload a chest x-ray image.')
 
@@ -108,17 +138,20 @@ def load_eye_disease_transfer_learning_model():
     if eye_disease_image is not None:
         st.image(eye_disease_image,caption='Uploaded image')
 
-        predicted_class = predict_eye_disease_tf(eye_disease_image,eye_disease_model)
-        if(predicted_class == 0):
-            st.error("The image is predicted as Cataract.")
-        elif(predicted_class == 1):
-            st.error("The image is predicted as Diabetic Retinopethy.")
-        elif(predicted_class == 2):
-            st.error("The image is predicted as Glaucoma.")
-        elif(predicted_class == 3):
-            st.success('The image is predicted as Normal.')
+        # checking image class whether it is chest X-ray image
+        predict_image_class = pre_img_class(eye_disease_image,image_classification_model)
+        if predict_image_class == 1:
+            predicted_class = predict_eye_disease_tf(eye_disease_image,eye_disease_model)
+            if(predicted_class == 0):
+                st.error("The image is predicted as Cataract.")
+            elif(predicted_class == 1):
+                st.error("The image is predicted as Diabetic Retinopethy.")
+            elif(predicted_class == 2):
+                st.error("The image is predicted as Glaucoma.")
+            elif(predicted_class == 3):
+                st.success('The image is predicted as Normal.')
         else:
-            st.warning("Unable to determine the prediction.")
+            st.warning("Unable to determine the prediction. Because uploaded image is not eye image.")
         
     else:
         st.warning('Please upload a eye-disease image.')
